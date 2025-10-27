b7117de4506961114f3c63807c6288e093a7c120
Victor Vieira <victor.a.vieira2014@gmail.com>
2025-10-27 12:57:22 -0400

    Manipulate Strings, with array char

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        char[] charArray = {'b','i','r','t','h',' ','d','a','y'};
+        String s = new String("hello");
+
+        String s1 = new String();
+        String s2 = new String(s);
+        String s3 = new String(charArray);
+        String s4 = new String(charArray,6,3);
+
+        System.out.printf("s1 = %s%ns2 = %s%ns3 = %s%ns4 = %s%n", s1, s2, s3, s4);
+    }
+}
